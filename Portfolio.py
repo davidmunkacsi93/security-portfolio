b88b6faeeebfa4f40695aa5eea54efcc97c640a5
David Munkacsi <david.munkacsi93@gmail.com>
2021-03-24 20:14:24 +0100

    Extend portfolio item to calculate returns.

--- Portfolio.py
+++ Portfolio.py
@@ -10,21 +10,12 @@ class Portfolio:
         self.securityData = pd.DataFrame()
         
         for portfolioItem in portfolioItems:
-            self.securityData[portfolioItem.ticker] = wb.DataReader(portfolioItem.ticker, data_source='yahoo', start='2010-1-1')['Adj Close']
+            self.securityData[portfolioItem.ticker] = wb.DataReader(portfolioItem.ticker, data_source='yahoo', start='2016-1-1')['Adj Close']
             
     def initializeDictionaries(self, portfolioItems: [PortfolioItem]):
         self.portfolioCounts = dict((item.ticker, item.shareCount) for item in portfolioItems)
         print(self.portfolioCounts)
                                         
-    def getReturns(self):
+    def calculateReturns(self):
         securityReturns : pd.core.frame.DataFrame = np.log(self.securityData / self.securityData.shift(1))
         return securityReturns
-    
-    def getReturnsCorrelation(self):
-        return self.getReturns().corr()
-
-    def getVariance(self):
-        return
-    
-    def getVolatility(self):
-        return
--- PortfolioItem.py
+++ PortfolioItem.py
@@ -1,4 +1,23 @@
+import pandas as pd
+from pandas_datareader import data as wb
+import numpy as np
+
 class PortfolioItem:
     def __init__(self, ticker: str, shareCount: int):
         self.ticker = ticker
         self.shareCount = shareCount
+        self.stockData = wb.DataReader(ticker, data_source='yahoo', start='2015-1-1', end='2021-03-23')['Adj Close']
+
+
+    def calculateSecurityReturn(self):
+        return np.log(self.stockData / self.stockData.shift(1))
+
+    def calculateBeta(self):
+        securityReturn = self.calculateSecurityReturn()
+        covarianceMatrix = securityReturn.cov() * 250
+
+    def calculateExpectedReturnCAPM(self):
+        equityRiskPremium = 0.5
+        riskFree = 0.025
+        
+        return
